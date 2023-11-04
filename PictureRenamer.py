dbfcb4cf368bd7d81773289b1e5eadf7e94dce21
mbedez <97473568+mbedez@users.noreply.github.com>
2023-11-04 22:46:17 +0100

    Update PictureRenamer.py

--- PictureRenamer.py
+++ PictureRenamer.py
@@ -1,8 +1,6 @@
-import tkinter as tk
-from tkinter import filedialog
+import os
 from PIL import Image
 from PIL.ExifTags import TAGS
-import os
 
 def get_photo_date(image_path):
     try:
@@ -17,32 +15,34 @@ def get_photo_date(image_path):
         pass
     except Exception as e:
         print("Erreur lors de la récupération des métadonnées EXIF :", str(e))
-    
+
     return None
 
 def rename_photo_with_date(image_path, date):
     directory, filename = os.path.split(image_path)
-    new_filename = date.replace(":", "-", 2).replace(":", "h", 1).replace(":", "m", 1).replace(" ", "_") + "s.jpg"
+    file_extension = filename.split(".")[-1].lower()  # Get the file extension
+    new_filename = date.replace(":", "-", 2).replace(":", "h", 1).replace(":", "m", 1).replace(" ", "_") + "s." + file_extension
     new_path = os.path.join(directory, new_filename)
-    
+
     if image_path != new_path:
         counter = 1
         while os.path.exists(new_path):
-            new_filename = date.replace(":", "-", 2).replace(":", "h", 1).replace(":", "m", 1).replace(" ", "_") + "s_" + str(counter) + ".jpg"
+            new_filename = date.replace(":", "-", 2).replace(":", "h", 1).replace(":", "m", 1).replace(" ", "_") + "s_" + str(counter) + "." + file_extension
             new_path = os.path.join(directory, new_filename)
             counter += 1
-        
+
         os.rename(image_path, new_path)
         return new_path
     else:
         return image_path
 
 def select_folder():
-    folder_path = filedialog.askdirectory()
+    folder_path = input("Veuillez entrer le chemin du dossier contenant les images : ")
     if folder_path:
         for filename in os.listdir(folder_path):
-            if filename.endswith(".jpg"):
-                file_path = os.path.join(folder_path, filename)
+            file_path = os.path.join(folder_path, filename)
+            file_extension = filename.split(".")[-1].lower()  # Get the file extension
+            if file_extension in ["jpg", "jpeg", "png", "webp"]:
                 photo_date = get_photo_date(file_path)
                 if photo_date is not None:
                     new_path = rename_photo_with_date(file_path, str(photo_date))
@@ -51,16 +51,22 @@ def select_folder():
                     print("Impossible de récupérer la date de la photo pour : " + file_path)
         print("Renommage terminé.")
     else:
-        print("Aucun dossier sélectionné.")
+        print("Aucun chemin de dossier spécifié.")
 
-# Création de la fenêtre principale
-window = tk.Tk()
-window.title("Sélection de dossier")
-window.geometry("300x150")
+if os.name == 'nt':  # Check if the OS is Windows
+    import tkinter as tk
+    from tkinter import filedialog
 
-# Bouton de sélection de dossier
-select_button = tk.Button(window, text="Sélectionner un dossier", command=select_folder)
-select_button.pack(pady=20)
+    # Création de la fenêtre principale
+    window = tk.Tk()
+    window.title("Sélection de dossier")
+    window.geometry("300x150")
 
-# Lancement de la boucle principale de l'interface graphique
-window.mainloop()
+    # Bouton de sélection de dossier
+    select_button = tk.Button(window, text="Sélectionner un dossier", command=select_folder)
+    select_button.pack(pady=20)
+
+    # Lancement de la boucle principale de l'interface graphique
+    window.mainloop()
+else:
+    select_folder()
