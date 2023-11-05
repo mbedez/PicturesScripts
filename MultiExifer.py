fbb267dcff7e272d60fc07549fd53a8d360e6110
mbedez <97473568+mbedez@users.noreply.github.com>
2023-11-05 01:15:46 +0100

    Update MultiExifer.py

--- MultiExifer.py
+++ MultiExifer.py
@@ -40,16 +40,16 @@ day = int(input("Entrez le jour de la prise de photo : "))
 month = int(input("Entrez le mois de la prise de photo : "))
 year = int(input("Entrez l'année de la prise de photo : "))
 
+s = 0
+m = 0
 for image_path in image_paths:
     # Récupérez le nom du fichier à partir du chemin
     image_filename = image_path.split('/')[-1]
 
     with open(image_path, 'rb') as image_file:  # Utilisez 'rb' pour le mode lecture
         my_image = Image(image_file)
-        
+
         print(f"\n{image_filename}")
-
-
 
         # Modifier les informations de l'image
         my_image.gps_latitude = gps_latitude_dms
@@ -57,10 +57,17 @@ for image_path in image_paths:
         my_image.gps_latitude_ref = gps_latitude_ref
         my_image.gps_longitude_ref = gps_longitude_ref
 
-        new_date = datetime(year=year, month=month, day=day, hour=0, minute=0, second=0)
+        new_date = datetime(year=year, month=month, day=day, hour=0, minute=m, second=s)
         my_image.datetime_original = new_date.strftime("%Y:%m:%d %H:%M:%S")
 
         with open(image_path, 'wb') as new_image_file:
             new_image_file.write(image_file.read())  # Copiez les données de l'image d'origine
             new_image_file.write(my_image.get_file())  # Ajoutez les informations EXIF
             print(f"Les informations ont été modifiées et l'image a été sauvegardée sous le nom {image_filename}.")
+            if s < 59:
+                s += 1
+            else :
+                s = 0
+                m += 1
+
+print("\nScript achevé avec succès !\n")
